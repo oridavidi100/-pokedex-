b959d15b8973b5d1955f8ce653cc33c3deb46129
oridavidi100 <89572548+oridavidi100@users.noreply.github.com>
2021-10-15 04:00:44 +0300

    '

--- src/index.js
+++ src/index.js
@@ -1,32 +1,83 @@
-document.getElementById("img").addEventListener("onmouseenter",changeSrc)//when the mouse over change the image
-document.getElementById("img").addEventListener("onmouseleave",backSrc)//when the mouse leave change the image again
+document.getElementById("img").addEventListener("mouseover",changeSrc)//when the mouse over change the image
+document.getElementById("img").addEventListener("mouseleave",backSrc)//when the mouse leave change the image again
 document.getElementById("searchButton").addEventListener("click",pokemonSearch);//search button
+//seraching by submit from input
 async function  pokemonSearch (){
-    let search =document.getElementById("search").value
+    try{
+        let search =document.getElementById("search").value
+        const respone= await axios.get(`https://pokeapi.co/api/v2/pokemon/${search}/`)
+        const data=await respone;
+        const name=data.data.name;
+        const Height=data.data.height;
+        const Weight=data.data.weight;
+        document.getElementById("Types").innerText=""
+        let Types=document.getElementById("Types");
+        for(let typeindex of data.data.types){
+            const typ =document.createElement("span")
+            typ.innerText=typeindex.type.name +" "
+            typ.addEventListener("click", typeClick)
+            Types.appendChild(typ)
+        }
+        const src=data.data.sprites.front_default
+        document.getElementById("name").innerText="Name :" +name;
+        document.getElementById("Height").innerText="Height :" +Height;
+        document.getElementById("Weight").innerText="Weight :" +Weight;
+        document.getElementById("img").setAttribute("src",src);
+        document.getElementById("img").setAttribute("alt",search);
+    }
+    catch{
+        return alert ("not found")
+    }
+}
+//change the img by putting the mouse over
+async function changeSrc(){
+    let search =event.target.alt
+    const respone= await axios.get(`https://pokeapi.co/api/v2/pokemon/${search}/`)
+    const data=await respone;
+    document.getElementById("img").setAttribute("src",data.data.sprites.back_default)
+}
+//changing the image back when the mouse isnt over 
+async function backSrc(){
+    let search =event.target.alt
+    const respone= await axios.get(`https://pokeapi.co/api/v2/pokemon/${search}/`)
+    const data=await respone;
+    document.getElementById("img").de
+    document.getElementById("img").setAttribute("src",data.data.sprites.front_default)
+}
+//print all pokemons with the same type
+async function typeClick(){
+    let type=(event.target.textContent) 
+    const respone= await axios.get(`https://pokeapi.co/api/v2/type/${type}`)
+    const data=await respone;
+    let list=document.getElementById("pokeList")
+    for (let poke of data.data.pokemon){
+        let li=document.createElement("li")
+        li.innerText=poke.pokemon.name
+        li.addEventListener("click",searchByClick)
+        list.appendChild(li)
+
+    }
+}
+//by clicking the name he comes up in the web 
+async function searchByClick(){
+    let search =event.target.textContent
     const respone= await axios.get(`https://pokeapi.co/api/v2/pokemon/${search}/`)
     const data=await respone;
     const name=data.data.name;
     const Height=data.data.height;
     const Weight=data.data.weight;
-    const Types=data.data.types[0].type.name
+    document.getElementById("Types").innerText=""
+    let Types=document.getElementById("Types");
+    for(let typeindex of data.data.types){
+        const typ =document.createElement("span")
+        typ.innerText=typeindex.type.name +" "
+        typ.addEventListener("click", typeClick)
+        Types.appendChild(typ)
+    }
     const src=data.data.sprites.front_default
-    document.getElementById("name").innerText="Name :" +name
-    document.getElementById("Height").innerText="Height :" +Height
-    document.getElementById("Weight").innerText="Weight :" +Weight
-    document.getElementById("Types").innerText="Types :" +Types
-    document.getElementById("img").setAttribute("src",src)
-    console.log(data)
-}
-async function changeSrc(){
-    let search =document.getElementById("search").value
-    const respone= await axios.get(`https://pokeapi.co/api/v2/pokemon/${search}/`)
-    const data=await respone;
-    document.getElementById("img").setAttribute("src",data.data.sprites.back_default)
-}
-
-async function backSrc(){
-    let search =document.getElementById("search").value
-    const respone= await axios.get(`https://pokeapi.co/api/v2/pokemon/${search}/`)
-    const data=await respone;
-    document.getElementById("img").setAttribute("src",data.data.sprites.front_default)
-}
+    document.getElementById("name").innerText="Name :" +name;
+    document.getElementById("Height").innerText="Height :" +Height;
+    document.getElementById("Weight").innerText="Weight :" +Weight;
+    document.getElementById("img").setAttribute("src",src);
+    document.getElementById("img").setAttribute("alt",search);
+    }
