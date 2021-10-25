11cd723017f787560ea301b44716ee2995862e65
ori davidi <oridavidi100@gmail.com>
2021-10-25 11:43:47 +0300

    ם

---
+++ src/login.js
@@ -0,0 +1,6 @@
+let submit=document.getElementById("submitUserName")
+submit.addEventListener("click",take)
+ function take(){
+    let username=document.getElementById("username").value
+    console.log(username)
+}
